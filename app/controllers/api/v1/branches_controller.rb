a8dc857d7d7f5480270a18e5a808f7dd5d3a0e3f
abarnes26 <abarnes26@yahoo.com>
2018-04-04 16:23:07 -0600

    endpoints for find all, find, and post created

--- app/controllers/api/v1/branches_controller.rb
+++ app/controllers/api/v1/branches_controller.rb
@@ -4,10 +4,24 @@ class Api::V1::BranchesController < ApplicationController
     render json: Branch.all
   end
 
+  def show
+    render json: Branch.find(params[:id])
+  end
+
+  def create
+    @branch = Branch.new(branch_params)
+    if @branch.save
+      # BranchUser.create(user: branch_params[:user_id], branch: @branch)
+      render json: "Branch was successfully created!"
+    else
+      render json: "Branch was not created, please try again"
+    end
+  end
+
   private
-    # def branch_params
-    #   params.require(:branch).permit(:id, :user_id, )
-    # end
+    def branch_params
+      params.require(:branch).permit(:id, :user_id, :street, :city, :state, :zipcode)
+    end
 
 
 end
--- spec/requests/api/v1/branch_requests_spec.rb
+++ spec/requests/api/v1/branch_requests_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Branches API" do
-  context "HTTP GET" do
+  context "HTTP GET all" do
     it "sends a list of customers" do
       create_list(:branch, 3)
 
@@ -12,6 +12,50 @@ describe "Branches API" do
       branches = JSON.parse(response.body)
 
       expect(branches.count).to eq(3)
+    end
+  end
+
+  context "HTTP GET one" do
+    it "sends a list of customers" do
+      branch_1 = create(:branch, street: "123 Solar Way")
+      create(:branch, street: "123 Energy Drive")
+      create(:branch, street: "123 Sunny Blvd")
+
+      get "/api/v1/branches/#{branch_1.id}"
+
+      expect(response).to be_success
+
+      branch = JSON.parse(response.body)
+
+      expect(branch['street']).to eq("123 Solar Way")
+    end
+  end
+
+  context "HTTP POST" do
+    it "creates a new branch" do
+
+      expect(Branch.all.count).to eq(0)
+
+      post "/api/v1/branches?branch[street]=123%20Solar%20Way&branch[city]=Denver&branch[state]=CO&branch[zipcode]=80202"
+
+      expect(response).to be_success
+      expect(response.body).to eq("Branch was successfully created!")
+      expect(Branch.all.count).to eq(1)
+      expect(Branch.first.street).to eq("123 Solar Way")
+    end
+  end
+
+  context "HTTP PATCH" do
+    it "updates an existing branch" do
+
+      expect(Branch.all.count).to eq(0)
+
+      patch "/api/v1/branches/1?branch[street]=123%20Solar%20Way"
+
+      expect(response).to be_success
+      expect(response.body).to eq("Branch was successfully created!")
+      expect(Branch.all.count).to eq(1)
+      expect(Branch.first.street).to eq("123 Solar Way")
     end
   end
 end
